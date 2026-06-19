b60132b8c9ce52630235e33b6b1d672eceeb47a4
Snarg <notme3029@gmail.com>
2026-06-19 20:08:40 +0200

    trooper navigation

---
+++ environment/environment.gd
@@ -0,0 +1,34 @@
+class_name Env #Environment was taken :(
+extends Node2D
+
+static var INST: Env
+
+@export var tilemap: TileMapLayer
+
+const directions := [Vector2i.RIGHT, Vector2i.DOWN, Vector2i.LEFT, Vector2i.UP]
+const end_tile_type := 4
+
+##name of the tileset property Type, which is basically just an id that tells troopers what to do
+const name_type := "Type"
+
+func _ready() -> void:
+	assert(INST == null)
+	INST = self
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_PREDELETE:
+		assert(INST == self)
+		INST = null
+
+##returns the new target position in global or the given position if its an end tile
+func move_target_from_global(global_pos: Vector2) -> Vector2:
+	var tile := tilemap.local_to_map(tilemap.to_local(global_pos))
+	var tile_data := tilemap.get_cell_tile_data(tile)
+	var idx: int = tile_data.get_custom_data(name_type)
+	
+	if idx == end_tile_type:
+		return global_pos
+	
+	#could walk the path until you reach a tile with a different direction so that the trooper doesnt have to check all the time.
+	#also trust that the tiles are set up correctly but that should be super obvious during development because the map would consistently crash
+	return tilemap.to_global(tilemap.map_to_local(tile + directions[idx]))
---
+++ troopers/trooper.gd
@@ -0,0 +1,32 @@
+class_name Trooper
+extends Node2D
+
+@export var move_speed: float = 10.
+
+#minimum distance towards target
+@export var min_dist_to_target: float = 0.1
+
+var target_pos: Vector2
+var walk_normal: Vector2
+
+func _ready() -> void:
+	target_pos = global_position
+	walk_normal = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	var new_pos := global_position + walk_normal * move_speed * delta
+	
+	if (global_position.distance_squared_to(target_pos) < global_position.distance_squared_to(new_pos) #this check makes sure we dont move past the point
+	|| global_position.distance_squared_to(target_pos) < min_dist_to_target * min_dist_to_target):
+		global_position = target_pos
+		target_pos = Env.INST.move_target_from_global(target_pos)
+		
+		#this shouldnt fail due to rounding errors because in move_target_from_global were returning the exact vector we pass in
+		if target_pos == global_position:
+			print("TROOPER FINISHED")
+			queue_free()
+		
+		walk_normal = global_position.direction_to(target_pos)
+		new_pos = global_position + walk_normal * move_speed * delta
+	
+	global_position = new_pos
