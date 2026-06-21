15ac33d88bdfee4b8cc098f76cafcd8c1334d1db
aughckt <aughckt@gmail.com>
2026-06-21 23:36:29 +0200

    tower sprites

--- towers/gun_tower/gun_tower.gd
+++ towers/gun_tower/gun_tower.gd
@@ -3,9 +3,13 @@ extends GenericTower
 
 const SHOOT_FORCE: float = 100.0
 var bullet_scene: PackedScene = preload("res://towers/gun_tower/bullet.tscn")
+@onready var shooter_sprite: AnimatedSprite2D = %AnimatedShooter
+
+
 
 
 func shoot() -> void:
+	shooter_sprite.play("shoot_up")
 	var bullet: Bullet = Bullet.create()
 	bullet.reparent.call_deferred(Env.INST.bullets)
 	bullet.dir = (Vector2.UP).rotated(rotation)
