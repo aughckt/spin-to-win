fde544c2aa95b12e6678b6d3ab1dc42ca1a58676
aughckt <aughckt@gmail.com>
2026-06-22 19:10:11 +0200

    multilane system

--- structure/level.gd
+++ structure/level.gd
@@ -3,17 +3,17 @@ extends Node2D
 
 @export_category("Fixed variables")
 @export var env: Env
-@export var spawn_point: Node2D
+@export var spawn_points: Node2D
 
 @export_category("Custom stuff")
 @export var level_hp: int = 20
 @export var wave_list: Array[Curve]
 @export var starting_budget: int = 10
+@export var lane_amount: int = 1
 
 
-func get_spawn_point() -> Vector2:
-	return spawn_point.global_position
-
+func _ready() -> void:
+	assert(wave_list.size() % lane_amount == 0)
 
 func set_build_phase(value: bool) -> void:
 	env.is_build_phase = value
